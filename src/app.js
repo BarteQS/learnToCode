0f46add1022d05a1069cde1c0021f12cf92c10a2
Bartosz Utratny <utratny.bartosz@gmail.com>
2023-01-29 02:12:27 +0100

    Selected all form elements

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const firstName = document.forms["productForm"]["firstName"].value;
+const lastName = document.forms["productForm"]["lastName"].value;
+const email = document.forms["productForm"]["email"].value;
+const password = document.forms["productForm"]["password"].value;
+
